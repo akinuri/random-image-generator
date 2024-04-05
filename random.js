ee7a821ddbf460bec4294808a9c319f92701efd3
Noreh AD <akinoreh@gmail.com>
2024-04-05 15:42:19 +0300

    Random.pick(): bound fix, return item if 1

--- random.js
+++ random.js
@@ -1,10 +1,13 @@
 class Random {
 
-    static pick(list, count) {
+    static pick(list, count = 1) {
         let picks = [];
         for (let i = 0; i < count; i++) {
-            let randomIndex = Math.round(Math.random() * list.length) - 1;
+            let randomIndex = Math.round(Math.random() * (list.length - 1));
             picks.push(list[randomIndex]);
+        }
+        if (count == 1) {
+            picks = picks[0];
         }
         return picks;
     }
