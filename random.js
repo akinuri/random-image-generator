d11764ed9b5f5b4f5b06d1024d15e42161492a21
Noreh AD <akinoreh@gmail.com>
2024-04-05 15:41:11 +0300

    trivial

--- random.js
+++ random.js
@@ -1,16 +1,16 @@
 class Random {
-    
+
     static pick(list, count) {
-        const picks = [];
+        let picks = [];
         for (let i = 0; i < count; i++) {
             let randomIndex = Math.round(Math.random() * list.length) - 1;
             picks.push(list[randomIndex]);
         }
         return picks;
     }
-    
+
     static color() {
         return "#" + Random.pick("0123456789ABCDEF", 6).join("");
     }
-    
+
 }
